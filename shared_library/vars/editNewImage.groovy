84005af796951e8171174f07d8ffbd994daed732
Alikhamed <alikhames566@gmail.com>
2024-06-11 00:07:17 +0300

    nn

--- shared_library/vars/editNewImage.groovy
+++ shared_library/vars/editNewImage.groovy
@@ -9,7 +9,7 @@ def call(String githubToken, String imageName, String gitUserEmail, String gitUs
                     sed -i "s|image:.*|image: ${imageName}:${BUILD_NUMBER}|g" k8s/deployment.yml
                     git add k8s/deployment.yml
                     git commit -m "Update deployment image to version ${BUILD_NUMBER}"
-                    git push https://${GITHUB_TOKEN}@github.com/${gitUserName}/${gitRepoName} HEAD:test
+                    git push https://${GITHUB_TOKEN}@github.com/${gitUserName}/${gitRepoName} HEAD:dev
                    '''
     
 
